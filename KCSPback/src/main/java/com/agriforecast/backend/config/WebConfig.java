2b272ad20c510a27c15035a021f320417b4ccd87
qs2698 <qkrtkdgur152@gmail.com>
2026-05-05 17:25:02 +0900

    backend쪽 수정

--- KCSPback/src/main/java/com/agriforecast/backend/config/WebConfig.java
+++ KCSPback/src/main/java/com/agriforecast/backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // Vite 기본 포트
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
